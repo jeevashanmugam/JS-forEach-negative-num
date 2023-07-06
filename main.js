a37ca5ebbd07abb808ba52f9ed0ebcec0964d8c3
jeevashanmugam <jeevasj2000@gmail.com>
2023-07-06 21:03:53 +0530

    JS-forEach-negative-num

---
+++ main.js
@@ -0,0 +1,18 @@
+let arr =[-23,-20,-17, -12, -5, 0, 1, 5, 12, 19, 20];
+
+
+document.write("Positive value : ")
+arr.forEach(function(value,index,array){
+
+  if(value>=0){
+    document.write(value)
+  }
+});
+
+
+// for(i=0;i<arr.length;i++){
+// 	if(arr[i]>=0){
+//   document.write("Positive value ")
+//   }
+
+// }
